eecb05f6f08c9ed96b0ac01d4ee4a46a4a90f6fb
Cailean Horton <caileanhorton@hotmail.com>
2024-02-29 14:47:22 -0600

    Added CSV parsed data to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 bad_product = Product.create(title: "Test")
 
 if not(bad_product.valid?)
@@ -10,9 +12,31 @@ end
 
 Product.delete_all
 
-676.times do
-  new_product = Product.create(
-    title: Faker::Commerce.product_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 1, to: 100))
+# 676.times do
+#   new_product = Product.create(
+#     title: Faker::Commerce.product_name,
+#     price: Faker::Commerce.price,
+#     stock_quantity: Faker::Number.between(from: 1, to: 100))
+# end
+
+filename = Rails.root.join("db/products.csv")
+
+puts "Loading products from the CSV file: #{filename}"
+
+csv_data = File.read(filename)
+products = CSV.parse(csv_data, headers: true, encoding: "utf-8")
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product["category"])
+
+  if category && category.valid?
+    product = category.products.create(
+      title: product["name"],
+      price: product["price"],
+      stock_quantity: product["stock quantity"],
+      description: product["description"],
+    )
+  end
 end
+
+puts "Created #{Product.count} products."
